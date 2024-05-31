044125b80ee196061b403f8fa588153b923a4358
alexMolex <aleksej-krivenko@mail.ru>
2024-05-31 16:38:00 +0300

    fix lint

--- src/app/(tabs)/Settings.tsx
+++ src/app/(tabs)/Settings.tsx
@@ -1,46 +1,46 @@
-import { Image, StyleSheet } from 'react-native';
-import { HelloWave } from '@/src/components/HelloWave';
-import ParallaxScrollView from '@/src/components/ParallaxScrollView';
-import { ThemedText } from '@/src/components/ThemedText';
-import { ThemedView } from '@/src/components/ThemedView';
+import { Image, StyleSheet } from 'react-native'
+import { HelloWave } from '@/src/components/HelloWave'
+import ParallaxScrollView from '@/src/components/ParallaxScrollView'
+import { ThemedText } from '@/src/components/ThemedText'
+import { ThemedView } from '@/src/components/ThemedView'
 
 export default function Settings() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/src/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	return (
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+			headerImage={
+				<Image
+					source={require('@/src/assets/images/partial-react-logo.png')}
+					style={styles.reactLogo}
+				/>
+			}
+		>
+			<ThemedView style={styles.titleContainer}>
+				<ThemedText type="title">Welcome!</ThemedText>
+				<HelloWave />
+			</ThemedView>
+			<ThemedView style={styles.stepContainer}>
+				<ThemedText type="subtitle">Step 1: Try it</ThemedText>
+			</ThemedView>
+		</ParallaxScrollView>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	titleContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
+		position: 'absolute',
+	},
+})
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,24 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const App = () => {
-  return (
-    <SafeAreaProvider>
-      <StatusBar style="auto" />
+	return (
+		<SafeAreaProvider>
+			<StatusBar style="auto" />
 
-      <RootLayout />
-    </SafeAreaProvider>
-  );
-};
+			<RootLayout />
+		</SafeAreaProvider>
+	)
+}
 
 const RootLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
-};
+	return (
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen name="+not-found" />
+		</Stack>
+	)
+}
 
-export default App;
+export default App
